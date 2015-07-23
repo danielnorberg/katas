4b7acd407014cefaff3afb8cd4d2dc5120dc1536
Daniel Norberg <dano@spotify.com>
2015-07-23 18:20:31 +0900

    quicksort

---
+++ src/main/java/quicksort/QuickSort.java
@@ -0,0 +1,46 @@
+package quicksort;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static java.util.Collections.swap;
+
+public class QuickSort {
+
+  static <T extends Comparable<T>> void quickSort(final List<T> values) {
+    quickSort(values, 0, values.size());
+  }
+
+  static <T extends Comparable<T>> List<T> quickSorted(final List<T> values) {
+    final List<T> sorted = new ArrayList<>(values);
+    quickSort(sorted);
+    return sorted;
+  }
+
+  private static <T extends Comparable<T>> void quickSort(final List<T> values, final int a, final int b) {
+    if (b - a <= 1) {
+      return;
+    }
+    final int p = partition(values, a, b);
+    quickSort(values, a, p);
+    quickSort(values, p, b);
+  }
+
+  private static <T extends Comparable<T>> int partition(final List<T> values, final int a, final int b) {
+    final int n = b - a;
+    final int p = a + ThreadLocalRandom.current().nextInt(n);
+    final T pv = values.get(p);
+    int si = a;
+    swap(values, b - 1, p);
+    for (int i = a; i < b - 1; i++) {
+      final T v = values.get(i);
+      if (v.compareTo(pv) < 0) {
+        swap(values, i, si);
+        si++;
+      }
+    }
+    swap(values, si, b - 1);
+    return si;
+  }
+}
---
+++ src/test/java/quicksort/QuickSortTest.java
@@ -0,0 +1,52 @@
+package quicksort;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import static com.google.common.primitives.Ints.asList;
+import static java.util.Collections.singletonList;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static quicksort.QuickSort.quickSorted;
+
+public class QuickSortTest {
+
+  @Test
+  public void testEmpty() throws Exception {
+    assertThat(quickSorted(Collections.<Integer>emptyList()), is(Collections.<Integer>emptyList()));
+  }
+
+  @Test
+  public void testSingleton() throws Exception {
+    assertThat(quickSorted(singletonList(1)), is(singletonList(1)));
+  }
+
+  @Test
+  public void testTwoElements() throws Exception {
+    assertThat(quickSorted(asList(1, 2)), is(asList(1, 2)));
+    assertThat(quickSorted(asList(2, 1)), is(asList(1, 2)));
+  }
+
+  @Test
+  public void testThreeElements() throws Exception {
+    assertThat(quickSorted(asList(1, 2, 3)), is(asList(1, 2, 3)));
+    assertThat(quickSorted(asList(1, 3, 2)), is(asList(1, 2, 3)));
+    assertThat(quickSorted(asList(2, 1, 3)), is(asList(1, 2, 3)));
+    assertThat(quickSorted(asList(2, 3, 1)), is(asList(1, 2, 3)));
+    assertThat(quickSorted(asList(3, 1, 2)), is(asList(1, 2, 3)));
+    assertThat(quickSorted(asList(3, 2, 1)), is(asList(1, 2, 3)));
+  }
+
+  @Test
+  public void testLargeList() throws Exception {
+    final List<Integer> input = asList(new Random(4711).ints(10000).toArray());
+    final List<Integer> output = quickSorted(input);
+    final List<Integer> expected = input.stream().sorted().collect(Collectors.toList());
+    assertThat(output, is(expected));
+  }
+
+}
