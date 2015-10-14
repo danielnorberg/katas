29121a68f32b48a539c61955c1f7925b22413ef4
Daniel Norberg <dano@spotify.com>
2015-10-14 16:58:47 +0900

    fix count twos

--- src/main/java/ctci/ch18/Q18_4_CountTwos.java
+++ src/main/java/ctci/ch18/Q18_4_CountTwos.java
@@ -1,17 +1,48 @@
 package ctci.ch18;
 
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import static java.lang.Integer.MAX_VALUE;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class Q18_4_CountTwos {
 
-  public static void main(String[] args) {
-    assertThat(pow(10, 0), is(1));
-    assertThat(pow(10, 1), is(10));
-    assertThat(pow(10, 2), is(100));
+  public static void main(String[] args) throws RunnerException {
     assertThat(countTwosBruteForce(25), is(9));
     assertThat(countTwosByDigit(25), is(9));
+    assertThat(countTwosByDigit(4711), is(2441));
+
+    // Fuzz
+    for (int i = 0; i < 100; i++) {
+      final int x = ThreadLocalRandom.current().nextInt(100_000);
+      final int expected = countTwosBruteForce(x);
+      final int twos = countTwosByDigit(x);
+      assertThat(twos, is(expected));
+    }
+
+    // Benchmark
+    Options opt = new OptionsBuilder()
+        .include(".*" + Q18_4_CountTwos.class.getSimpleName() + ".*")
+        .forks(1)
+        .warmupIterations(5)
+        .measurementIterations(5)
+        .build();
+    new Runner(opt).run();
   }
+
+  @Benchmark
+  public int benchmarkCountTwosByDigit() {
+    final int x = ThreadLocalRandom.current().nextInt(MAX_VALUE);
+    return countTwosByDigit(x);
+  }
+
 
   private static int countTwosBruteForce(final int n) {
     int count = 0;
@@ -27,51 +58,38 @@ public class Q18_4_CountTwos {
     return count;
   }
 
+  /**
+   * Count twos per digit.
+   *
+   * O(log n)
+   */
   private static int countTwosByDigit(final int n) {
-    int count = 0;
-    int d = 0;
+    int twos = 0;
     int x = n;
+    int z = 0;
+    int i = 0;
+    int pow10ip = 0;
+    int pow10i = 1;
     while (x > 0) {
-      final int digit = x % 10;
-      // d == 0: x == 25
-      // d == 0: x / 10 == 2
-      // d == 0: y == 2 * 1 == 2
-      // d == 0: digit == 5
+      final int a = x / 10;
+      final int b = x - 10 * a;
+      final int c = z + b * pow10i;
 
-      // d == 1: x == 2
-      // d == 1: y == 0
-      // d == 1: digit = 2
-
-      final int y = (x / 10) * pow(10, d);
-      final int twos;
-      if (digit < 2) {
-        twos = y;
-      } else if (digit == 2) {
-        // d == 1: n == 25
-        // d == 1: x == 2
-        // d == 1: pow(10, d) == pow(10, 1) == 10
-        // d == 1: twosInRange == 1 + 25 - 2 * 10 == 1 + 25 - 20 == 6 (20, 21, 22, 23, 24, 25)
-        // d == 1: twos == 0 + 6 == 6
-        final int twosInRange = 1 + n - x * pow(10, d);
-        twos = y + twosInRange;
-      } else {
-        // d == 0: twos == 2 + 1 == 3
-        twos = y + 1;
+      twos += b * i * pow10ip;
+      if (b == 2) {
+        twos += 1 + z;
+      } else if (b > 2) {
+        twos += pow10i;
       }
-      // d == 0: count == 3
-      // d == 1: count == 3 + 6 == 9
-      count += twos;
-      x /= 10;
-      d++;
-    }
-    return count;
-  }
 
-  private static int pow(final int x, final int n) {
-    int pow = 1;
-    for (int j = 0; j < n; j++) {
-      pow *= x;
+      x = a;
+      z = c;
+
+      i++;
+
+      pow10ip = pow10i;
+      pow10i *= 10;
     }
-    return pow;
+    return twos;
   }
 }
